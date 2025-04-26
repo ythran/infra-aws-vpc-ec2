7574f2a348d83f9f45501dbdfcd0385e823ae614
ythran <gabrielcouto616@gmail.com>
2025-04-26 05:30:31 -0300

    Create main.tf
    Adicionando main.tf inicial com VPC, Subnet e Internet Gateway

---
+++ main.tf
@@ -0,0 +1,56 @@
+# Provider da AWS
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC Principal
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-lab"
+  }
+}
+
+# Subnet Pública
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet-lab"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "internet-gateway-lab"
+  }
+}
+
+# Route Table Pública
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table-lab"
+  }
+}
+
+# Associação da Subnet com a Route Table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
